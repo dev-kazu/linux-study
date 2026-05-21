453b23c19de3a125c44b9efa96e752edfac63a09
dev-kazu <tsuruda.kazuya.md6@eng.nipponsteel.com>
2026-05-21 16:48:22 +0900

    Add user input practice

--- python-practice/hello.py
+++ python-practice/hello.py
@@ -2,6 +2,5 @@ print("hello world")
 name = "kazuya"
 print("My name is {name}")
 "2026-5-20"
--uv で仮想環境を作成した
-ーsource .venv/bin/activeで仮想環境を有効化した
--hello.pyを実行した
+name = input("名前を入力してください:")
+print(f"Hello, {name}")
