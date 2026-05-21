e520f3934f5c92afe7add62cab09ec3e0cfd4a18
dev-kazu <tsuruda.kazuya.md6@eng.nipponsteel.com>
2026-05-21 16:52:05 +0900

    Add conditional branching practice

--- python-practice/hello.py
+++ python-practice/hello.py
@@ -3,4 +3,9 @@ name = "kazuya"
 print("My name is {name}")
 "2026-5-20"
 name = input("名前を入力してください:")
+age = int(input("年齢を入力してください:"))
 print(f"Hello, {name}")
+if age >= 20:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
